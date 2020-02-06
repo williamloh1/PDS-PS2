1eff650a4342d505c1ac5a875d89545a72471966
William Loh <william.loh@wustl.edu>
2020-02-06 11:20:19 -0600

    Update Problem Set 2.R
    done

--- Problem Set 2.R
+++ Problem Set 2.R
@@ -19,13 +19,66 @@ for (i in 1:1000) {
     test.sum = FALSE
   }
   if(i!=1 & test.sum==FALSE) {
-    #if(dice.rolls[1]==2 | dice.rolls[1]==6 | dice.rolls[2]==2 | dice.rolls[2]==6) {
-    #  break
-    #}
-    if(sum(dice.rolls)==2 | sum(dice.rolls)==6) {
+    if(dice.rolls[1]==2 | dice.rolls[1]==6 | dice.rolls[2]==2 | dice.rolls[2]==6) {
       break
     }
   }
 }
-print(save.count*2)
+print(save.count)
+
+#Problem 3
+GSS<-read.csv("~/Downloads/GSS-data.csv")
+vote.choice = function(x) {
+  if(x=="Trump") {
+    length(GSS$pres16[GSS$pres16=="Trump"])
+  }
+  else if(x=="Clinton") {
+    length(GSS$pres16[GSS$pres16=="Clinton"])
+  }
+  else if(x=="Other") {
+    length(GSS$pres16[GSS$pres16!="Trump" & GSS$pres16!="Clinton"])
+  }
+  else {
+    print("Please enter either ‘Trump’ ‘Clinton’ or ‘Other’ into the functionto return a valid response")
+  }
+}
+
+#Problem 4
+install.packages('fivethirtyeight')
+library(fivethirtyeight)
+cabinet_turnover <- subset(cabinet_turnover,is.na(cabinet_turnover$days)==FALSE)
+preslist <- c("Carter","Reagan","Bush 41","Clinton","Bush 43","Obama","Trump")
+presdayslist <- c(1461,2922,1461,2922,2922,2922,1105)
+presdays<-data.frame("presidents"=preslist,"days"=presdayslist)
+appoint <- function(pres) {
+  avgday.appointee<-mean(cabinet_turnover$days[cabinet_turnover$president==pres])
+  daysinoffice<-presdays$days[presdays$presidents==pres]
+  return(avgday.appointee/daysinoffice)
+}
+
+#Problem 5
+congress_stats <- function(input) {
+  if(input=="congress") {
+    congress <- c(80:113)
+    congressages<-c()
+    for(i in congress) {
+      avgage<-mean(congress_age$age[congress_age$congress==i])
+      congressages<-c(congressages,avgage)
+    }
+    congress.ages.acc<-data.frame("Congress"=congress,"Average Age"=congressages)
+    return(congress.ages.acc)
+  }
+  if(input=="state") {
+    states <- unique(congress_age$state)
+    congressages<-c()
+    for(i in states) {
+      avgage<-mean(congress_age$age[congress_age$state==i])
+      congressages<-c(congressages,avgage)
+    }
+    state.ages.acc<-data.frame("State"=states,"Average Age"=congressages)
+    return(state.ages.acc)
+  }
+}
+
+
 
